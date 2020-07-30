633083c4d29495ab49b6a5540d87777e28626bbd
Michael Meyer <michael.c.meyer26@gmail.com>
2020-07-30 11:48:26 -0500

    Finished for loops exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -5,8 +5,27 @@ function showMultiplicationTable(value) {
     }
 }
 
-var random = Math.floor((Math.random() * 200) + 20);
-
-function randomEvenOrOdd(value) {
-
+function randomEvenOrOdd() {
+    for(var x = 0; x <= 10; x++) {
+        var random = Math.floor((Math.random() * 200) + 20);
+        var state = (random % 2 === 0 ? "even" : "odd");
+        console.log(random + " is " + state);
+    }
 }
+
+function numberPyramid() {
+    for (var x = 1; x < 10; x++) {
+        var pyramidTier = x.toString();
+        for (var y = 1; y < x; y++) {
+            pyramidTier += x;
+        }
+        console.log(pyramidTier);
+    }
+}
+
+function fivesCountdown() {
+    for (var x = 100; x > 0; x-= 5) {
+        console.log(x);
+    }
+}
+
