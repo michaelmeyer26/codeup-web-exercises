06673dfd227dee3034c3f9ac5efc272bb9c0cc7e
Michael Meyer <michael.c.meyer26@gmail.com>
2020-07-30 14:19:12 -0500

    Completed while exercise

--- js/while.js
+++ js/while.js
@@ -8,9 +8,18 @@ function timesTwo () {
 
 function iceCreamSalesperson () {
     var allCones = Math.floor(Math.random() * 50) + 50;
+    console.log("Total cones today is " + allCones);
     do {
-
+        var thisCustomer = Math.floor(Math.random() * 5) + 1;
+        if (thisCustomer <= allCones) {
+            console.log("This customer wants " + thisCustomer);
+            allCones -= thisCustomer;
+            console.log(thisCustomer + " cones sold...");
+            console.log(allCones + " cones left.")
+        } else if (thisCustomer > allCones) {
+            console.log("I cannot sell you " + thisCustomer + " cones. I only have " + allCones + ".");
+        }
     } while (allCones > 0);
+    console.log("Yay! I sold them all!");
 }
 
-//Math.floor(Math.random() * 5) + 1;
