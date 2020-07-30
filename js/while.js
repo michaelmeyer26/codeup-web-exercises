d549fc9eb27aac9ac250a4b1b4c3384e44c3426f
Michael Meyer <michael.c.meyer26@gmail.com>
2020-07-30 14:26:40 -0500

    Finished while exercise

--- js/while.js
+++ js/while.js
@@ -12,12 +12,12 @@ function iceCreamSalesperson () {
     do {
         var thisCustomer = Math.floor(Math.random() * 5) + 1;
         if (thisCustomer <= allCones) {
-            console.log("This customer wants " + thisCustomer);
+            //console.log("This customer wants " + thisCustomer);
             allCones -= thisCustomer;
             console.log(thisCustomer + " cones sold...");
             console.log(allCones + " cones left.")
         } else if (thisCustomer > allCones) {
-            console.log("I cannot sell you " + thisCustomer + " cones. I only have " + allCones + ".");
+            console.log("I cannot sell you " + thisCustomer + " cones. I only have " + allCones);
         }
     } while (allCones > 0);
     console.log("Yay! I sold them all!");
