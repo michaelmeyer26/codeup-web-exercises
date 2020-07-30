4e9fc6d2175b5a2a7f2babe38f7fb2d334dfecf2
Michael Meyer <michael.c.meyer26@gmail.com>
2020-07-30 13:51:14 -0500

    finished breal_and_continue

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,14 +1,16 @@
 function skipIt () {
     while (true) {
-        var input = prompt("Please enter an even number between 1 and 50.");
+        var input = prompt("Please enter an odd number between 1 and 50.");
         if (input <= 50 && input >=1 && input % 2 === 1) {
             break;
         }
     }
+    console.log("Number to skip is: " + input);
     for (var x = 1; x <= 50; x++) {
-        if (x % 2 === 0 || x === input) {
+        if (input == x || x % 2 === 0) {
             continue;
         }
         console.log("Here is an odd number: " + x);
     }
 }
+
