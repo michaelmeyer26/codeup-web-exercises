d7c82b14708f6da9d821731ea82197c99c33d8b0
Michael Meyer <michael.c.meyer26@gmail.com>
2020-07-30 14:08:20 -0500

    Initial work on while.js

---
+++ js/while.js
@@ -0,0 +1,16 @@
+function timesTwo () {
+    var num = 1;
+    while (num < 65536) {
+        num *= 2;
+        console.log(num);
+    }
+}
+
+function iceCreamSalesperson () {
+    var allCones = Math.floor(Math.random() * 50) + 50;
+    do {
+
+    } while (allCones > 0);
+}
+
+//Math.floor(Math.random() * 5) + 1;
