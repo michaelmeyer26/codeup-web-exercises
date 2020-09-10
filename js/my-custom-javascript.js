4b364db9bf385640edc42beaf57e802568deea91
Michael Meyer <michael.c.meyer26@gmail.com>
2020-09-10 10:08:31 -0500

    added dblclick function

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -2,8 +2,12 @@
 
 $(document).ready(function() {
     $( 'h1' ).click(function() {
-        $(this).css("background-color", "red")
-        $(this).css("color", "white")
+        $(this).css("background-color", "red");
+        $(this).css("color", "white");
+    });
+
+    $( 'p' ).dblclick(function() {
+       $(this).css("font-size", "18px");
     });
 });
 
