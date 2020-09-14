3ee1c534c6b64db60b859af85f597f788e7c29c1
Michael Meyer <michael.c.meyer26@gmail.com>
2020-09-14 13:49:34 -0500

    properly parsed data for tools website

---
+++ js/JSON-practice.js
@@ -0,0 +1,24 @@
+var bash = {
+    "name": "Bash",
+    "num_students": 2,
+    "location": {
+        "state": "TX",
+        "city": "Dallas"
+    },
+    "students": [
+        {
+            "name": "Stacy R",
+            "dob": "1990-01-24",
+            "scholarship": true,
+            "capstone": null
+        },
+        {
+            "name": "Juan M",
+            "dob": "1992-05-13",
+            "scholarship": false,
+            "capstone": null
+        }
+    ],
+    "remote": false,
+    "instructors": ["Fernando M", "Douglas H"]
+}
